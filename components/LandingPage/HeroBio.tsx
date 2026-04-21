745e65a319932609069ab905a8f0a9712f2b331a
taqui-786 <mdtaqui.jhar@gmail.com>
2026-04-21 19:27:12 +0530

    Fix: Tech stack badge sizing

--- components/LandingPage/HeroBio.tsx
+++ components/LandingPage/HeroBio.tsx
@@ -13,40 +13,40 @@ import {
 
 function HeroBio() {
   return (
-    <div className="text-base md:text-lg text-primary leading-10 "> i build
+    <div className="text-base md:text-lg text-primary leading-10 ">i build
       {" "}<b className="text-title ">Full Stack apps</b> with{" "}
       <CustomBadge href="https://nextjs.org/" name="Next.js">
-        <NextjsIcon size={18} />
+        <NextjsIcon size={14} />
       </CustomBadge>{" "}
       and{" "}
       <CustomBadge href="https://www.typescriptlang.org/" name="TypeScript">
-        <TypescriptIcon size={18} />
+        <TypescriptIcon size={14} />
       </CustomBadge>
       , using{" "}
       <CustomBadge href="https://www.prisma.io/" name="Prisma">
-        <PrismaIcon size={18} />
+        <PrismaIcon size={14} />
       </CustomBadge>
       ,{" "}
       <CustomBadge href="https://www.postgresql.org/" name="PostgreSQL">
-        <PostgresqlIcon size={18} />
+        <PostgresqlIcon size={14} />
       </CustomBadge>
       , and{" "}
       <CustomBadge href="https://socket.io/" name="Socket.IO">
-        <SocketioIcon size={18} />
+        <SocketioIcon size={14} />
       </CustomBadge>
       , with animations via{" "}
       <CustomBadge href="https://www.framer.com/motion/" name="Framer Motion">
-        <FramermotionIcon size={18} />
+        <FramermotionIcon size={14} />
       </CustomBadge>
       {" "} or{" "}
       <CustomBadge href="https://gsap.com/" name="GSAP" />
       , deployed on{" "}
       <CustomBadge href="https://vercel.com/" name="Vercel">
-        <VercelIcon size={18} />
+        <VercelIcon size={14} />
       </CustomBadge>{" "}
       and{" "}
       <CustomBadge href="https://aws.amazon.com/" name="AWS">
-        <AwsIcon size={18} />
+        <AwsIcon size={14} />
       </CustomBadge>
       .
     </div>
--- components/ui/custom-badge.tsx
+++ components/ui/custom-badge.tsx
@@ -11,10 +11,10 @@ export const CustomBadge = ({
     <a
       target="_blank"
       style={{ textDecoration: "none" }}
-      className="inline-flex  items-center no-underline text-sm bg-muted  border border-dashed dark:border-white/30 border-black/20 py-[3px] px-[6px] rounded-[6px] skill-inner-shadow self-end text-primary overflow-hidden"
+      className="inline-flex h-6 items-center no-underline  bg-muted  border border-dashed dark:border-white/30 border-black/20 py-0.5 px-[6px] rounded-[6px] skill-inner-shadow self-end text-primary overflow-hidden"
       href={href}
     >
-      {children && <div className=" shrink-0">{children}</div>}
+      {children && <div className="shrink-0 ">{children}</div>}
       <p className={`${children ? "ml-1" : ""} text-sm font-bold`}>{name}</p>
     </a>
   );
