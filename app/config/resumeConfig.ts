7f8170cecc86b8fda857e9b73d269606436ef7f1
taqui-786 <mdtaqui.jhar@gmail.com>
2026-06-13 22:17:26 +0530

    feat: Updated resume

--- app/config/resumeConfig.ts
+++ app/config/resumeConfig.ts
@@ -1,3 +1,3 @@
 export const resumeConfig = {
-    url: "https://drive.google.com/file/d/171j5dQjGCIaV2qtUk_TvCF2yLJ5LWpwI/preview",
+    url: "https://drive.google.com/file/d/1lLzSybrzr4BjpyOXhEzGsXVB6RNguxew/preview",
 }
